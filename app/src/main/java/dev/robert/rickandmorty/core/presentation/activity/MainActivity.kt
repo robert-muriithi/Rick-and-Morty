6ec11bb4b7d6baf0349b7f1b4f1f477064afc8f3
Robaa <njuguiinirobert@gmail.com>
2023-04-09 12:04:06 +0300

    plant timber for logging

--- app/src/main/java/dev/robert/rickandmorty/core/presentation/activity/MainActivity.kt
+++ app/src/main/java/dev/robert/rickandmorty/core/presentation/activity/MainActivity.kt
@@ -1,4 +1,4 @@
-package dev.robert.rickandmorty
+package dev.robert.rickandmorty.core.presentation.activity
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -10,8 +10,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import dev.robert.rickandmorty.ui.theme.RickAndMortyTheme
+import dagger.hilt.android.AndroidEntryPoint
+import dev.robert.rickandmorty.core.presentation.designsystem.theme.RickAndMortyTheme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
