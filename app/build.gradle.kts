c5dcbe1af61292847e86f5095774050838dc1ac6
Robaa <njuguiinirobert@gmail.com>
2023-04-17 08:07:07 +0300

    add constraint layout dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,6 +61,7 @@ dependencies {
     implementation ("androidx.compose.ui:ui-tooling-preview:1.2.0")
     implementation ("androidx.compose.material3:material3:1.0.0-alpha02")
     implementation("androidx.compose.material:material:1.4.1")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation ("junit:junit:4.13.2")
     androidTestImplementation ("androidx.test.ext:junit:1.1.5")
     androidTestImplementation ("androidx.test.espresso:espresso-core:3.5.1")
