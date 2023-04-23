79a5f351a50428360e2eb8601f95a031a160726b
Robaa <njuguiinirobert@gmail.com>
2023-04-23 17:42:23 +0300

    minor

--- app/src/main/java/dev/robert/rickandmorty/core/presentation/activity/MainActivity.kt
+++ app/src/main/java/dev/robert/rickandmorty/core/presentation/activity/MainActivity.kt
@@ -6,10 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import dagger.hilt.android.AndroidEntryPoint
 import dev.robert.rickandmorty.core.presentation.designsystem.theme.RickAndMortyTheme
 import dev.robert.rickandmorty.feature.characters.presentation.characters.CharactersMainScreen
