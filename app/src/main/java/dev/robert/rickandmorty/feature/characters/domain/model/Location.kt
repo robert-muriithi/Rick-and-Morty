0c4e26976c69c87a0669b63e586928074a6c0723
Robaa <njuguiinirobert@gmail.com>
2023-04-23 17:40:30 +0300

    model character response

---
+++ app/src/main/java/dev/robert/rickandmorty/feature/characters/domain/model/Location.kt
@@ -0,0 +1,6 @@
+package dev.robert.rickandmorty.feature.characters.domain.model
+
+data class Location(
+    val name: String,
+    val url: String
+)
---
+++ app/src/main/java/dev/robert/rickandmorty/feature/characters/domain/model/Origin.kt
@@ -0,0 +1,6 @@
+package dev.robert.rickandmorty.feature.characters.domain.model
+
+data class Origin(
+    val name: String,
+    val url: String
+)
